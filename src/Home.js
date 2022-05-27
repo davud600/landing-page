141405e1f63ec63bf2fe880cb4b0adc9badad177
davud600 <davudgorqi@gmail.com>
2022-05-27 16:26:56 +0300

    second-commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <div>
+      <div className="main-section">
+
+      </div>
+      <div className="courses-section">
+
+      </div>
+      <div className="subscribe-section">
+
+      </div>
+    </div>
+  )
+}
